88e56992870b8095c59582164de66030afc4c87b
Adam Mohiuddin <adam@AdamMBPT.local>
2018-09-15 18:53:06 -0400

    DeckOrHand: Created DeckOrHand class with makeDeck()

---
+++ src/main/java/blackjack/DeckOrHand.java
@@ -0,0 +1,19 @@
+package blackjack;
+
+import java.util.ArrayList;
+
+public class DeckOrHand {
+	private ArrayList<Card> cards;
+	
+	public DeckOrHand() {
+		this.cards = new ArrayList<Card>();
+	}
+	
+	public void makeDeck() {
+		for (Suit suit : Suit.values()) {
+			for (Rank rank : Rank.values()) {
+				this.cards.add(new Card(suit, rank));
+			}	
+		}
+	}
+}
