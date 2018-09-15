b5088a2de5c6819673878b8d453af85b2d9d36b8
Adam Mohiuddin <adam@AdamMBPT.local>
2018-09-15 19:35:47 -0400

    DeckOrHand: Added suit and rank lists and changed makeDeck()

--- src/main/java/blackjack/DeckOrHand.java
+++ src/main/java/blackjack/DeckOrHand.java
@@ -1,18 +1,22 @@
 package blackjack;
 
 import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
 public class DeckOrHand {
 	private ArrayList<Card> cards;
+	final List<String> suit = Arrays.asList("D", "S", "C", "H");
+	final List<String> rank = Arrays.asList("2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K", "A");
 	
 	public DeckOrHand() {
 		this.cards = new ArrayList<Card>();
 	}
 	
 	public void makeDeck() {
-		for (Suit suit : Suit.values()) {
-			for (Rank rank : Rank.values()) {
-				this.cards.add(new Card(suit, rank));
+		for (String s : suit) {
+			for (String r : rank) {
+				this.cards.add(new Card(s, r));
 			}	
 		}
 	}
