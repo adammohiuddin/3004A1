9a22c5bdfe308a4349ba1b6c9d9a610646d8615a
Adam Mohiuddin <adam@AdamMBPT.local>
2018-09-18 22:27:16 -0400

    Blackjack: Added createDeckAndUI()

--- src/main/java/blackjack/Blackjack.java
+++ src/main/java/blackjack/Blackjack.java
@@ -1,8 +1,15 @@
 package blackjack;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.ArrayList;
+import java.util.Scanner;
+
 public class Blackjack {
 	
 	private static final int maxHandValue = 21;
+	private static Scanner fileScanner;
+	
 	
 	public static DeckOrHand didBlackjackHappen(DeckOrHand playerHand, DeckOrHand dealerHand) {
 		boolean playerWin = false;
@@ -43,5 +50,52 @@ public class Blackjack {
 			return null;
 		}
 	}
+	
+	
+	
+	public static void createDeckAndPickUI(DeckOrHand deck, ArrayList<String> playerMovesFromFile, Scanner playerConsole) {
+		
+		System.out.println("Hello! Thank you for choosing AJNM blackjack!");
+		System.out.println("Would you like to play on the console (c) or by file (f)?");
+		
+		ArrayList<String> cardsFromFile;
+		String command = playerConsole.nextLine();
+		
+		if (command.startsWith("f")) {
+			
+			System.out.println("Please enter the filename in src/main/resources (ex: filename.txt)");
+			command = "src/main/resources/" + playerConsole.nextLine();
+
+			try {
+				fileScanner = new Scanner(new File(command));
+				fileScanner.useDelimiter(" ");
+			} catch (FileNotFoundException e) {
+				System.out.println("Could not open filename entered. Please try again.");
+				e.printStackTrace();
+			}
+			
+			String tokenFromFile;
+			cardsFromFile = new ArrayList<>();
+			
+			while (fileScanner.hasNext()) {
+				tokenFromFile = fileScanner.next();
+				if (tokenFromFile.length() > 1) {
+					cardsFromFile.add(tokenFromFile);
+				} else {
+					playerMovesFromFile.add(tokenFromFile);
+				}
+			}
+
+			deck.makeDeckFromFile(cardsFromFile);
+			fileScanner.close();
+			
+		}
+		else if (command.startsWith("c")) {
+			deck.makeDeck();
+			deck.shuffleDeck();
+		}	
+	}
+	
+
 	
 }
